518ce2431327efafbcf86b7c7bedc5a33c30b6ce
Maycon Carlete <maycon.carlete@gmail.com>
2022-09-13 09:14:13 -0300

    refactor: move data from struct to class

--- src/cpp/Main.cpp
+++ src/cpp/Main.cpp
@@ -1,17 +1,12 @@
-#include "Application.h"
-
+#include "./Physics/Vec2.h"
+#include <memory>
+#include <iostream>
 int main(int argc, char *args[]) {
-    Application app;
+    // Application app;
+    std::unique_ptr<Vec2> vec2 = std::make_unique<Vec2>(6, 8);
+    std::unique_ptr<Vec2> vec3 = std::make_unique<Vec2>(4, 5);
 
-    app.Setup();
-
-    while (app.IsRunning()) {
-        app.Input();
-        app.Update();
-        app.Render();
-    }
-
-    app.Destroy();
+    vec2->Print();
 
     return 0;
 }
