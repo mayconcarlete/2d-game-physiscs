afcd2661dd3954e4d18037d7c7a503599f6746c0
Maycon Carlete <maycon.carlete@gmail.com>
2022-09-13 18:42:53 -0300

    feat: refactor from struct to class

--- src/cpp/Physics/Vec2.cpp
+++ src/cpp/Physics/Vec2.cpp
@@ -53,121 +53,96 @@ Vec2& Vec2::Normalize(){
   return *this;
 }
 
-// Vec2 Vec2::Rotate(const float angle) const {
-//   Vec2 Result;
-//   Result.x = x * cos(angle) - y * sin(angle);
-//   Result.y = x * sin(angle) + y * cos(angle);
-//   return Result;
-// }
+Vec2 Vec2::UnitVector() const {
+  Vec2 Result = Vec2(0 ,0);
+  float length = this->Magnitude();
+  if(length != 0.0){
+    Result.x = this->x / length;
+    Result.y = this->y / length;
+  }
+  return Result;
+}
+Vec2 Vec2::Normal() const {
+  return Vec2(this->y, -(this->x)).Normalize();
+}
 
-// float Vec2::Magnitude() const {
-//   return sqrtf(x * x + y * y);
-// }
+float Vec2::Dot(const Vec2& v) const {
+  return (this->x * v.x + this->y * v.y);
+}
 
-// float Vec2::MagnitudeSquared() const {
-//   return (x * x + y * y);
-// }
+float Vec2::Cross(const Vec2& v) const {
+  return (this->x * v.y) - (this->y * v.x);
+}
 
-// Vec2& Vec2::Normalize(){
-//   float length = Magnitude();
-//   if(length != 0.0){
-//     x /= length;
-//     y /= length;
-//   }
-//   return *this;
-// }
+Vec2& Vec2::operator =  (const Vec2& v){
+  x = v.x;
+  y = v.y;
+  return *this;
+}
 
-// Vec2 Vec2::UnitVector() const {
-//   float length = Magnitude();
-//   Vec2 result = Vec2(0, 0);
-//   if(length != 0.0){
-//     result.x = x / length;
-//     result.y = y / length;
-//   }
-//   return result;
-// }
+bool Vec2::operator == (const Vec2& v) const {
+  return x == v.x && y == v.y;
+}
 
-// Vec2 Vec2::Normal() const {
-//   return Vec2(y, -x).Normalize();
-// }
+bool Vec2::operator != (const Vec2& v) const {
+  return !(*this == v);
+}
 
-// float Vec2::Dot(const Vec2& v) const {
-//   return (x * v.x) + (y * v.y);
-// }
+Vec2 Vec2::operator + (const Vec2& v) const {
+  Vec2 result;
+  result.x = x + v.x;
+  result.y = y + v.y;
+  return result;
+}
 
-// float Vec2::Cross(const Vec2& v) const {
-//   return (x * v.y) - (y * v.x);
-// }
+Vec2 Vec2::operator - (const Vec2& v) const {
+  Vec2 result;
+  result.x = x - v.x;
+  result.y = y - v.y;
+  return result;
+}
 
-// Vec2& Vec2::operator =  (const Vec2& v){
-//   x = v.x;
-//   y = v.y;
-//   return *this;
-// }
+Vec2 Vec2::operator * (const float n) const {
+  Vec2 result;
+  result.x = x * n;
+  result.y = y * n;
+  return result;
+}
 
-// bool Vec2::operator == (const Vec2& v) const {
-//   return x == v.x && y == v.y;
-// }
+Vec2 Vec2::operator / (const float n) const {
+  Vec2 result;
+  result.x = x / n;
+  result.y = y / n;
+  return result;
+}
 
-// bool Vec2::operator != (const Vec2& v) const {
-//   return !(*this == v);
-// }
+Vec2& Vec2::operator += (const Vec2& v) {
+  x += v.x;
+  y += v.y;
+  return *this;
+}
 
-// Vec2 Vec2::operator + (const Vec2& v) const {
-//   Vec2 result;
-//   result.x = x + v.x;
-//   result.y = y + v.y;
-//   return result;
-// }
+Vec2& Vec2::operator -= (const Vec2& v){
+  x -= v.x;
+  y -= v.y;
+  return *this;
+}
 
-// Vec2 Vec2::operator - (const Vec2& v) const {
-//   Vec2 result;
-//   result.x = x - v.x;
-//   result.y = y - v.y;
-//   return result;
-// }
+Vec2& Vec2::operator *= (const float n) {
+  x *= n;
+  y *= n;
+  return *this;
+}
 
-// Vec2 Vec2::operator * (const float n) const {
-//   Vec2 result;
-//   result.x = x * n;
-//   result.y = y * n;
-//   return result;
-// }
+Vec2& Vec2::operator /= (const float n){
+  x /= n;
+  y /= n;
+  return *this;
+}
 
-// Vec2 Vec2::operator / (const float n) const {
-//   Vec2 result;
-//   result.x = x / n;
-//   result.y = y / n;
-//   return result;
-// }
-
-// Vec2& Vec2::operator += (const Vec2& v) {
-//   x += v.x;
-//   y += v.y;
-//   return *this;
-// }
-
-// Vec2& Vec2::operator -= (const Vec2& v){
-//   x -= v.x;
-//   y -= v.y;
-//   return *this;
-// }
-
-// Vec2& Vec2::operator *= (const float n) {
-//   x *= n;
-//   y *= n;
-//   return *this;
-// }
-
-// Vec2& Vec2::operator /= (const float n){
-//   x /= n;
-//   y /= n;
-//   return *this;
-// }
-
-// Vec2 Vec2::operator - (){
-//   Vec2 result;
-//   result.x = x * -1;
-//   result.y = y * -1;
-//   return result;
-// }
+Vec2 Vec2::operator - (){
+  Vec2 result;
+  result.x = x * -1;
+  result.y = y * -1;
+  return result;
+}
--- src/cpp/Physics/Vec2.h
+++ src/cpp/Physics/Vec2.h
@@ -18,46 +18,25 @@ class Vec2 {
     float MagnitudeSquared() const;
 
     Vec2& Normalize();
+    Vec2 UnitVector() const;
+    Vec2 Normal() const;
+
+    float Dot(const Vec2& v) const;
+    float Cross(const Vec2& v) const;
+    Vec2& operator = (const Vec2& v); // v1 = v2
+    bool operator == (const Vec2& v) const; // v1 == v2
+    bool operator != (const Vec2& v) const; // v1 != v2
+
+    Vec2 operator + (const Vec2& v) const; // v1 + v2
+    Vec2 operator - (const Vec2& v) const; // v1 - v2
+    Vec2 operator * (const float n) const; // v1 * n
+    Vec2 operator / (const float n) const; // v1 / n
+    Vec2 operator - (); // -v1
+
+    Vec2& operator += (const Vec2& v); // v1 += v2
+    Vec2& operator -= (const Vec2& v); // v1 -= v2
+    Vec2& operator *= (const float n); // v1 *= n
+    Vec2& operator /= (const float n); // v1 /= n
 };
-
-// struct Vec2 {
-//   float x;
-//   float y;
-
-//   Vec2();
-//   Vec2(float x, float y);
-//   ~Vec2() = default;
-
-//   void Add(const Vec2 &v);
-//   void Sub(const Vec2 &v);
-//   void Scale(const float n);
-//   Vec2 Rotate(const float angle) const;
-
-//   float Magnitude() const;
-//   float MagnitudeSquared() const;
-
-//   Vec2& Normalize();
-//   Vec2 UnitVector() const;
-//   Vec2 Normal() const;
-
-//   float Dot(const Vec2& v) const;
-//   float Cross(const Vec2& v) const;
-
-
-//   Vec2& operator = (const Vec2& v); // v1 = v2
-//   bool operator == (const Vec2& v) const; // v1 == v2
-//   bool operator != (const Vec2& v) const; // v1 != v2
-
-//   Vec2 operator + (const Vec2& v) const; // v1 + v2
-//   Vec2 operator - (const Vec2& v) const; // v1 - v2
-//   Vec2 operator * (const float n) const; // v1 * n
-//   Vec2 operator / (const float n) const; // v1 / n
-//   Vec2 operator - (); // -v1
-
-//   Vec2& operator += (const Vec2& v); // v1 += v2
-//   Vec2& operator -= (const Vec2& v); // v1 -= v2
-//   Vec2& operator *= (const float n); // v1 *= n
-//   Vec2& operator /= (const float n); // v1 /= n
-// };
 
 #endif
