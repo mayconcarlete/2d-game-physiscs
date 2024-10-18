eedcb1c6be965b8a525a2c3f5a4a49a172ef01fa
Maycon Carlete <maycon.carlete@gmail.com>
2024-10-18 15:35:13 -0300

    feat: remove typescript

--- include/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define WINDOW_TITLE "SDL Game Loop"
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
-
-
-#endif
--- src/vec3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "vec3.h"
-
-struct Vec3 *new_vec3(double x, double y, double z){
-    
-}
-
