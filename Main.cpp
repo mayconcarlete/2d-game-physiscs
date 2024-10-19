ed597bb6ffe7f460f9168fe760843efbcc34ee7a
Maycon Carlete <maycon.carlete@gmail.com>
2024-10-18 23:15:57 -0300

    test

--- Main.cpp
+++ Main.cpp
@@ -4,11 +4,18 @@
 
 int main(){
 
-    Vec2 a = Vec2(2,2);
+    Vec2 a = Vec2(1,0);
     Vec2 b = Vec2(1, 1);
-    
-    a.Add(b);
-    a.Print();
+
+    // a.Add(b);
+    // a.Print();
+    // a.Sub(b);
+    // a.Print();
+    // a.Scale(4);
+    // a.Print();
+
+    // Vec2 res = a.Rotate(3.14/2);
+    // res.Print();
 
     return EXIT_SUCCESS;
 }
