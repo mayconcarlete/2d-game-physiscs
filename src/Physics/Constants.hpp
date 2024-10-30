9b0cb739af481a9b81e34ca965859b360c455655
Maycon Carlete <maycon.carlete@gmail.com>
2024-10-30 12:43:39 -0300

    refactor

--- src/Physics/Constants.hpp
+++ src/Physics/Constants.hpp
@@ -1,11 +1,13 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int FPS = 60;
-const int MILLISECONDS_PER_FRAME = 16; // "1000/60"
-const int PIXELS_PER_METER = 50;
+#include <cstdint>
 
+const std::uint32_t FPS = 60;
+const std::uint32_t MILLISECONDS_PER_FRAME = 16;
+const std::int32_t PIXELS_PER_METER = 50; // checar por que quando passamos isso para uint32_t as particulas bugam pegando velocidades altissimas.
 
-const int window_width = 500;
-const int window_height = 400;
+const std::uint32_t window_width = 500;
+const std::uint32_t window_height = 400;
+
 #endif
